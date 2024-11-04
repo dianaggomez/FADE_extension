4b9a7dc1be707861ca6ce9672c731da6d95870fa
Anusha Pathuri <170770299+anusha-pathuri@users.noreply.github.com>
2024-11-04 11:11:25 -0500

    fixed preprocess data, updated IncomeDataset

--- src/datasets.py
+++ src/datasets.py
@@ -1,37 +1,52 @@
+import os
+import sys
 import torch
 import pandas as pd
 from torch.utils.data import Dataset
 
 
+DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data")
+
 class IncomeDataset(Dataset):
-    
-    URLS = {
-        "train": 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data',
-        "test": 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test'
+    BASE_DIR = os.path.join(DATA_DIR, "adult")
+    PATHS = {
+        "train_basis": os.path.join(BASE_DIR, "train_basis.csv"),
+        "train_betahats": os.path.join(BASE_DIR, "train_betahats.csv"),
+        "test": os.path.join(BASE_DIR, "test.csv"),
+        "validate": os.path.join(BASE_DIR, "validate.csv"),
     }
     
-    COLUMNS = ['age', 'workclass', 'fnlwgt', 'education', 'education_num', 
-                'marital_status', 'occupation', 'relationship', 'race', 
-                'sex', 'capital_gain', 'capital_loss', 'hours_per_week', 
-                'native_country', 'income']
+    FEATURES = ["education_num", "age"]  # Define the features we want to use
+    TARGET = "income"
     
-    FEATURES = []  # Define the features we want to use
-    
-    def __init__(self, split="train", sensitive_attribute="sex"):
+    def __init__(self, split="train", sensitive_attribute="sex", features=None, to_tensor=True):
         """
         Args:
-            X: Input features
-            Y: Labels
-            S: Sensitive attributes
+            split: The dataset split, one of "train_basis", "train_betahats", "test", or "validate"
+            sensitive_attribute: The sensitive attribute to use as the protected class
+            features: The features to use in the dataset
         """
-        if split not in ["train", "test"]:
-            raise ValueError("Invalid split. Must be either 'train' or 'test'")
+        if split not in self.PATHS:
+            raise ValueError(f"Invalid split. Must be one of {list(self.PATHS.keys())}")
+        
+        if features is None:
+            features = self.FEATURES
             
-        data = pd.read_csv(self.URLS[split], names=self.COLUMNS, sep=',', skipinitialspace=True)
-        # self.X = data.drop(columns=['income', sensitive_attribute]).values
-        self.X = data[self.FEATURES].values
-        self.Y = (data['income'] > 50_000).astype(float).values
-        self.S = data[sensitive_attribute].values
+        self.S_name = sensitive_attribute
+        self.X_names = features
+        self.Y_name = self.TARGET
+        
+        data = pd.read_csv(self.PATHS[split])
+        self.S = data[[self.S_name]].values
+        self.X = data[self.X_names].values
+        self.Y = data[[self.Y_name]].values
+        
+        if to_tensor:
+            self.S = torch.tensor(self.S, dtype=torch.float32)
+            self.X = torch.tensor(self.X, dtype=torch.float32)
+            self.Y = torch.tensor(self.Y, dtype=torch.float32)
+            
+        self.is_tensor = to_tensor
 
     def __len__(self):
         """Return the total number of samples."""
@@ -43,4 +58,3 @@ class IncomeDataset(Dataset):
         y = self.Y[idx]
         s = self.S[idx]
         return x, y, s
-
